2c32e421e15ec4fe70ade82955abefc683a11b05
hamid814 <hamid331994@gmail.com>
2020-09-21 12:13:57 +0330

    dev

---
+++ client/build/precache-manifest.d3bf37c613cf5e7c78220581c37d9631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7206087099346787ddde379845649e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d562d19054144b80249f",
+    "url": "/static/css/main.a9c15fb3.chunk.css"
+  },
+  {
+    "revision": "650f567caa3536f7459c",
+    "url": "/static/js/2.107d765c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.107d765c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d562d19054144b80249f",
+    "url": "/static/js/main.105b47a1.chunk.js"
+  },
+  {
+    "revision": "e574e440225f0a78297e",
+    "url": "/static/js/runtime-main.ab994882.js"
+  }
+]);
